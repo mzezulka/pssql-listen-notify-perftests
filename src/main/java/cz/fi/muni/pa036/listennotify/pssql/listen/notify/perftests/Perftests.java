c5ca994b167a3765eba7747e5cc37f271bdb2aa9
Miloslav Žežulka <mzezulka@localhost.localdomain>
2020-04-26 13:08:50 +0200

    Refactoring.

--- src/main/java/cz/fi/muni/pa036/listennotify/pssql/listen/notify/perftests/Perftests.java
+++ src/main/java/cz/fi/muni/pa036/listennotify/pssql/listen/notify/perftests/Perftests.java
@@ -1,5 +1,6 @@
 package cz.fi.muni.pa036.listennotify.pssql.listen.notify.perftests;
 
+import cz.fi.muni.pa036.listennotify.api.CrudClient;
 import cz.fi.muni.pa036.listennotify.api.EventType;
 import cz.fi.muni.pa036.listennotify.api.ListenNotifyClient;
 import java.io.File;
@@ -16,6 +17,7 @@ import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.TearDown;
 import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.infra.Blackhole;
 
 /**
@@ -25,7 +27,9 @@ import org.openjdk.jmh.infra.Blackhole;
 @State(Scope.Thread)
 public class Perftests {
 
-    private ListenNotifyClient client = ListenNotifyClientFactory.client();
+    private CrudClient crudClient = CrudClientFactory.client();
+    private ListenNotifyClient listenNotifyClient = ListenNotifyClientFactory.client();
+    
     private int id = 1;
     private static final String DEFAULT_MESSAGE = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
     private static final String INSERT_TEXT_MESSAGE = "INSERT INTO text VALUES (%d, '%s');";
@@ -34,18 +38,18 @@ public class Perftests {
     @Setup(Level.Iteration)
     public void setup() throws SQLException {
         // clean all data
-        client.executeStatement("DELETE FROM bin;");
-        client.executeStatement("DELETE FROM text;");
+        crudClient.executeStatement("DELETE FROM bin;");
+        crudClient.executeStatement("DELETE FROM text;");
         id = 1;
         for (EventType e : EventType.values()) {
-            client.registerEventListener(e);
+            crudClient.registerEventListener(e);
         }
     }
 
     @TearDown
     public void cleanup() {
         for (EventType e : EventType.values()) {
-            client.deregisterEventListener(e);
+            crudClient.deregisterEventListener(e);
         }
     }
 
@@ -54,8 +58,8 @@ public class Perftests {
     @BenchmarkMode({Mode.AverageTime})
     public boolean insertNaive(Blackhole bh) {
         try {
-            client.executeStatement(String.format(INSERT_TEXT_MESSAGE, id++, DEFAULT_MESSAGE));
-            bh.consume(client.next());
+            crudClient.executeStatement(String.format(INSERT_TEXT_MESSAGE, id++, DEFAULT_MESSAGE));
+            bh.consume(listenNotifyClient.next());
             return true;
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
@@ -66,8 +70,8 @@ public class Perftests {
     @BenchmarkMode({Mode.AverageTime})
     public boolean insertBasic(Blackhole bh) {
         try {
-            client.insertText(id++, DEFAULT_MESSAGE);
-            bh.consume(client.next());
+            crudClient.insertText(id++, DEFAULT_MESSAGE);
+            bh.consume(listenNotifyClient.next());
             return true;
         } catch (SQLException ex) {
             throw new RuntimeException(ex);
@@ -94,8 +98,8 @@ public class Perftests {
     @BenchmarkMode({Mode.AverageTime})
     public boolean insertHundredsOfChars(Blackhole bh, HundredCharsState hcs) {
         try {
-            client.insertText(id++, hcs.fileContents);
-            bh.consume(client.next());
+            crudClient.insertText(id++, hcs.fileContents);
+            bh.consume(listenNotifyClient.next());
             return true;
         } catch(SQLException sqle) {
             throw new RuntimeException(sqle);
@@ -120,8 +124,8 @@ public class Perftests {
     @BenchmarkMode({Mode.AverageTime})
     public boolean insertHundredsOfThousandsOfChars(Blackhole bh, HundredsOfThousandsCharsState hcs) {
         try {
-            client.insertText(id++, hcs.fileContents);
-            bh.consume(client.next());
+            crudClient.insertText(id++, hcs.fileContents);
+            bh.consume(listenNotifyClient.next());
             return true;
         } catch(SQLException sqle) {
             throw new RuntimeException(sqle);
@@ -149,10 +153,10 @@ public class Perftests {
         try {
             // we need to insert text as well since the bin table
             // contains a foreign key (used in later scenarios)
-            client.insertText(id++, DEFAULT_MESSAGE);
-            bh.consume(client.next());
-            client.insertBinary(id, is.fis);
-            bh.consume(client.next());
+            crudClient.insertText(id++, DEFAULT_MESSAGE);
+            bh.consume(listenNotifyClient.next());
+            crudClient.insertBinary(id, is.fis);
+            bh.consume(listenNotifyClient.next());
             return true;
         } catch(SQLException sqle) {
             throw new RuntimeException(sqle);
@@ -166,14 +170,14 @@ public class Perftests {
     @BenchmarkMode({Mode.AverageTime})
     public boolean insertTextAndImageAndDelete(Blackhole bh, ImageState is) {
         try {
-            client.insertText(id++, DEFAULT_MESSAGE);
-            bh.consume(client.next());
-            client.insertBinary(id, is.fis);
-            bh.consume(client.next());
-            client.deleteText(id);
+            crudClient.insertText(id++, DEFAULT_MESSAGE);
+            bh.consume(listenNotifyClient.next());
+            crudClient.insertBinary(id, is.fis);
+            bh.consume(listenNotifyClient.next());
+            crudClient.deleteText(id);
             // note that we expect two messages to be reported, one for each table
-            bh.consume(client.next());
-            bh.consume(client.next());
+            bh.consume(listenNotifyClient.next());
+            bh.consume(listenNotifyClient.next());
             return true;
         } catch(SQLException sqle) {
             throw new RuntimeException(sqle);
@@ -182,11 +186,15 @@ public class Perftests {
     
     @State(Scope.Benchmark)
     public static class NotifyScatterUpdateState {
-        private int noIterations = 1000;
-        private ListenNotifyClient c = ListenNotifyClientFactory.client();
+        @Param({"10", "1000"})
+        private int noIterations;
+        // we need to make a fresh copy of the client because crudClient variable
+        // is static and cannot be accessed from a non-static context
+        private final CrudClient c = CrudClientFactory.client();
         
         @Setup(Level.Iteration)
         public void setup() {
+            c.registerEventListener(EventType.UPDATE_TEXT);
             for(int i = 0; i < noIterations; i++) {
                 try {
                     c.insertText(i++, DEFAULT_MESSAGE);
@@ -204,9 +212,9 @@ public class Perftests {
     @BenchmarkMode({Mode.SingleShotTime})
     public boolean oneSqlStatementMultipleNotify(Blackhole bh, NotifyScatterUpdateState nsus) {
         try {
-            client.executeStatement(UPDATE_TEXT);
+            crudClient.executeStatement(UPDATE_TEXT);
             for(int i = 0; i < nsus.noIterations; i++) {
-                bh.consume(client.next());    
+                bh.consume(listenNotifyClient.next());    
             }
             return true;
         } catch(SQLException sqle) {
